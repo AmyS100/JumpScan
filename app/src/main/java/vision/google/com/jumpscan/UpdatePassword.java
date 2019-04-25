f9094fe1d64950496d12a4099cae50b4f7e6d099
Amy Siggins <amymsiggins@hotmail.com>
2019-04-25 16:10:03 +0100

    fixed some bugs

---
+++ app/src/main/java/vision/google/com/jumpscan/UpdatePassword.java
@@ -0,0 +1,81 @@
+package vision.google.com.jumpscan;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UpdatePassword extends AppCompatActivity {
+
+    private Button update;
+    private EditText newPassword;
+    private FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_password);
+
+        update = (Button)findViewById(R.id.btnChangePassword);
+        newPassword = (EditText)findViewById(R.id.etNewPassword);
+
+        //Arrow mark at the top of the pages
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        //creating the new password
+        String userPasswordNew = newPassword.getText().toString();
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userPasswordNew = newPassword.getText().toString();
+                firebaseUser.updatePassword(userPasswordNew).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(UpdatePassword.this, "Password Update Changed", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else{
+                            Toast.makeText(UpdatePassword.this, "Password Update Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        //getting the new password and allowing the user to save it
+        firebaseUser.updatePassword(userPasswordNew).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(UpdatePassword.this, "Password Update Changed", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(UpdatePassword.this, "Password Update Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // when the arrow is clicked at the top it this is the code which will bring the page to the previous page
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
